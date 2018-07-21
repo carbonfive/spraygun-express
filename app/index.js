0b1b9ebd9995ab4ac15acd7829edb10eb61d3f42
Matt Brictson <mattb@carbonfive.com>
2018-07-20 17:46:11 -0700

    Install helmet (security best practices)

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const config = require("@app/config");
 
 const app = express();
-
+app.use(helmet());
 app.use(morgan(config.express.logFormat));
 
 app.get("/api/hello", (req, res) => {
